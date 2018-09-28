1954a8d321d570e01d2c4908cf0c679453c8db27
xiaoz <xiaoz93@outlook.com>
2018-09-28 22:46:44 +0800

    update

---
+++ README.md
@@ -0,0 +1,36 @@
+# CCAA
+
+CentOS + Caddy + Aria2 + AriaNg，实现离线下载、文件管理、在线播放等功能。
+
+
+
+### 主要功能
+
+* CentOS 一键安装Caddy + Aria2 + AriaNg
+* Aria2 提供离线下载
+* AriaNg提供WEB界面
+* Caddy filemanager提供文件管理
+
+### 环境要求
+
+* CentOS 6+ X64 (推荐使用CentOS 7)
+
+### 安装
+
+```bash
+yum -y install wget unzip curl
+wget https://github.com/helloxz/ccaa/archive/master.zip
+unzip master.zip && cd ccaa-master && sh ccaa.sh
+```
+
+
+
+### 鸣谢
+
+
+
+### 联系我
+
+* Blog: [https://www.xiaoz.me/](https://www.xiaoz.me/)
+* QQ: 337003006
+* 技术交流群: 147687134
--- ccaa.sh
+++ ccaa.sh
@@ -11,8 +11,8 @@ export PATH
 #安装Aria2
 function install_aria2(){
 	#更新软件
-	yum -y update
-	yum -y install wget unzip curl
+	#yum -y update
+	yum -y install curl
 	yum -y install epel-release
 	yum -y install aria2
 }
@@ -24,11 +24,14 @@ function install_caddy(){
 	wget http://soft.xiaoz.org/linux/caddy_v0.11.0_linux_amd64_custom_personal.tar.gz -O caddy.tar.gz
 	tar -zxvf caddy.tar.gz
 	mv caddy /usr/sbin/
-	chmod +x caddy
+	chmod +x /usr/sbin/caddy
 	#添加服务
 	mv init/linux-systemd/caddy.service /lib/systemd/system
 	chmod +x /lib/systemd/system/caddy.service
+	#开机启动
+	systemctl enable caddy.service
 }
+
 #处理配置文件
 function dealconf(){
 	#创建目录和文件
@@ -38,6 +41,8 @@ function dealconf(){
 	touch /etc/ccaa/caddy.log
 	cp aria2.conf /etc/ccaa/
 	cp caddy.conf /etc/ccaa/
+	chmod +x ccaa
+	cp ccaa /usr/sbin
 }
 #自动放行端口
 function chk_firewall() {
@@ -59,33 +64,86 @@ function chk_firewall() {
 }
 #设置账号密码
 function setting(){
+	echo '-------------------------------------------------------------'
 	read -p "设置下载路径（请填写绝对地址，如/data/aria2）:" downpath
 	read -p "设置Aria2密码(字母或数字组合，不要含有特殊字符):" secret
 	read -p "设置Caddy用户名:" caddyuser
 	read -p "设置Caddy密码:" caddypass
 
-	#执行操作
+	#执行替换操作
 	mkdir -p ${downpath}
+	sed -i "s%dir=%dir=${downpath}%g" /etc/ccaa/aria2.conf
 	sed -i "s/rpc-secret=/rpc-secret=${secret}/g" /etc/ccaa/aria2.conf
 	sed -i "s/username/${caddyuser}/g" /etc/ccaa/caddy.conf
 	sed -i "s/password/${caddypass}/g" /etc/ccaa/caddy.conf
+	sed -i "s%/home%${downpath}%g" /etc/ccaa/caddy.conf
+	sed -i "s%/admin%/admin ${downpath}%g" /etc/ccaa/caddy.conf
+
+	#安装AriaNg
+	wget http://soft.xiaoz.org/website/AriaNg.zip
+	unzip AriaNg.zip
+	cp -a AriaNg ${downpath}/AriaNg
 
 	#启动服务
 	nohup aria2c --conf-path=/etc/ccaa/aria2.conf > /etc/ccaa/aria2.log 2>&1 &
 	nohup caddy -conf="/etc/ccaa/caddy.conf" > /etc/ccaa/caddy.log 2>&1 &
 
+	#获取ip
+	osip=$(curl -s https://api.ip.sb/ip)
+
 	echo '-------------------------------------------------------------'
-	echo '大功告成，请访问:http://IP:6080'
+	echo "大功告成，请访问: http://${osip}:6080/AriaNg/"
+	echo '用户名:' ${caddyuser}
+	echo '密码:' ${caddypass}
+	echo 'Aria2 RPC 密钥:' ${secret}
 	echo '需要帮助请访问:'
 	echo '-------------------------------------------------------------'
 }
 
+#卸载
+function uninstall(){
+	#停止所有服务
+	kill -9 $(pgrep 'aria2c')
+	kill -9 $(pgrep 'caddy')
+
+	#删除服务
+	systemctl disable caddy.service
+	rm -rf /lib/systemd/system/caddy.service
+	#删除文件
+	rm -rf /etc/ccaa
+	rm -rf /usr/sbin/caddy
+	rm -rf /usr/sbin/ccaa
+
+	#卸载Aria2
+	yum -y remove aria2
+
+	#删除端口
+	if [ -e "/etc/sysconfig/iptables" ]
+	then
+		sed -i '/^.*6080.*/'d /etc/sysconfig/iptables
+		sed -i '/^.*6800.*/'d /etc/sysconfig/iptables
+		sed -i '/^.*6998.*/'d /etc/sysconfig/iptables
+		sed -i '/^.*51413.*/'d /etc/sysconfig/iptables
+		service iptables save
+		service iptables restart
+	else
+		firewall-cmd --zone=public --remove-port=6080/tcp --permanent
+		firewall-cmd --zone=public --remove-port=6800/tcp --permanent
+		firewall-cmd --zone=public --remove-port=6998/tcp --permanent
+		firewall-cmd --zone=public --remove-port=51413/tcp --permanent
+		firewall-cmd --reload
+	fi
+	echo "------------------------------------------------"
+	echo '卸载完成！'
+	echo "------------------------------------------------"
+}
+
 #选择安装方式
 echo "------------------------------------------------"
-echo "Caddy + Aria2 + AriaNg一键安装脚本，简称CCAA"
+echo "CentOS 7 + Caddy + Aria2 + AriaNg一键安装脚本，简称CCAA"
 echo "1) 安装CCAA"
 echo "2) 卸载CCAA"
-echo "3) 更新"
+#echo "3) 更新"
 echo "q) 退出！"
 read -p ":" istype
 case $istype in
@@ -97,10 +155,7 @@ case $istype in
     	setting
     ;;
     2) 
-    	check_os
-    	get_ip
-    	chk_firewall
-    	BinaryInstall
+    	uninstall
     ;;
     3) 
     	#执行卸载函数
