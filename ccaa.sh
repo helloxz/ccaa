c95a27a8a3d84bd96d47c833d718188b83fdb325
xiaoz <xiaoz93@outlook.com>
2020-02-28 14:03:51 +0800

    fix bug

--- ccaa.sh
+++ ccaa.sh
@@ -32,6 +32,7 @@ function setout(){
 		sudo apt-get install -y curl make bzip2 gzip wget unzip
 	fi
 	#创建临时目录
+	cd
 	mkdir ./ccaa_tmp
 	#进入临时目录
 	cd ./ccaa_tmp
@@ -44,20 +45,23 @@ function install_aria2(){
 	tar jxvf aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
 	cd aria2-1.35.0-linux-gnu-64bit-build1
 	make install
-	cd ..
+	cd
 }
 
 #安装File Browser文件管理器
 function install_file_browser(){
+	cd ./ccaa_tmp
 	#下载File Browser
 	wget https://github.com/filebrowser/filebrowser/releases/download/v2.1.0/linux-amd64-filebrowser.tar.gz
 	#解压
 	tar -zxvf linux-amd64-filebrowser.tar.gz
 	#移动位置
 	mv filebrowser /usr/sbin
+	cd
 }
 #处理配置文件
 function dealconf(){
+	cd ./ccaa_tmp
 	#下载CCAA项目
 	wget https://github.com/helloxz/ccaa/archive/master.zip
 	#解压
@@ -70,6 +74,7 @@ function dealconf(){
 	chmod +x /etc/ccaa/upbt.sh
 	chmod +x ccaa-master/ccaa
 	cp ccaa-master/ccaa /usr/sbin
+	cd
 }
 #自动放行端口
 function chk_firewall(){
