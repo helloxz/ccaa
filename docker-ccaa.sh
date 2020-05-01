fb69ed87d24e2d8945862bc78247efe92fb18f0d
xiaoz <xiaoz93@outlook.com>
2020-05-01 09:11:31 +0800

    fix bug

--- docker-ccaa.sh
+++ docker-ccaa.sh
@@ -11,7 +11,7 @@ export PATH
 aria2_url='https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2'
 filebrowser_url='https://github.com/filebrowser/filebrowser/releases/download/v2.0.16/linux-amd64-filebrowser.tar.gz'
 master_url='https://github.com/helloxz/ccaa/archive/master.zip'
-ccaa_web_url='http://soft.xiaoz.org/linux/ccaa_web.tar.gz'
+ccaa_web_url='http://soft.xiaoz.org/linux/ccaa_web'
 
 #安装前的检查
 function check(){
