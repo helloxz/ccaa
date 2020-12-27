67c853f55ee024a031bfb6030b72d4b84e316047
crazypeace <665889+crazypeace@users.noreply.github.com>
2020-12-27 23:09:58 +0800

    Update ccaa.sh

--- ccaa.sh
+++ ccaa.sh
@@ -36,18 +36,35 @@ else
 fi
 
 #CDN域名设置
-if [ $1 = 'cdn' ]
-	then
-	aria2_url='http://soft.xiaoz.top/linux/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2'
-	filebrowser_url='http://soft.xiaoz.top/linux/linux-amd64-filebrowser.tar.gz'
-	master_url='https://github.com/helloxz/ccaa/archive/master.zip'
-	ccaa_web_url='http://soft.xiaoz.top/linux/ccaa_web.tar.gz'
-	else
+args=$1
+if [[ -z $args ]]
+then
 	aria2_url='https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2'
 	filebrowser_url='https://github.com/filebrowser/filebrowser/releases/download/v2.0.16/linux-amd64-filebrowser.tar.gz'
 	master_url='https://github.com/helloxz/ccaa/archive/master.zip'
-	ccaa_web_url='http://soft.xiaoz.org/linux/ccaa_web.tar.gz'
+	ccaa_web_url='http://soft.xiaoz.org/linux/ccaa_web.tar.gz'	
+else
+	if [ $args = 'cdn' ]
+	then
+		aria2_url='http://soft.xiaoz.top/linux/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2'
+		filebrowser_url='http://soft.xiaoz.top/linux/linux-amd64-filebrowser.tar.gz'
+		master_url='https://github.com/helloxz/ccaa/archive/master.zip'
+		ccaa_web_url='http://soft.xiaoz.top/linux/ccaa_web.tar.gz'
+	fi
 fi
+
+# if [ $1 = 'cdn' ]
+	# then
+	# aria2_url='http://soft.xiaoz.top/linux/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2'
+	# filebrowser_url='http://soft.xiaoz.top/linux/linux-amd64-filebrowser.tar.gz'
+	# master_url='https://github.com/helloxz/ccaa/archive/master.zip'
+	# ccaa_web_url='http://soft.xiaoz.top/linux/ccaa_web.tar.gz'
+	# else
+	# aria2_url='https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2'
+	# filebrowser_url='https://github.com/filebrowser/filebrowser/releases/download/v2.0.16/linux-amd64-filebrowser.tar.gz'
+	# master_url='https://github.com/helloxz/ccaa/archive/master.zip'
+	# ccaa_web_url='http://soft.xiaoz.org/linux/ccaa_web.tar.gz'
+# fi
 
 #安装前的检查
 function check(){
