d5773701f9be5bd9c0aadf2b91edd53fa5297b95
crazypeace <txican@gmail.com>
2026-04-26 21:03:52 +0800

    Fix typo in echo command for File Browser username

--- ccaa.sh
+++ ccaa.sh
@@ -302,7 +302,7 @@ function setting(){
 	echo -e "大功告成，请访问: ${green}http://${osip}:6080/#!/settings/rpc/set/ws/${osip}/6800/jsonrpc/${secret_base64url}${none}"
 	echo "大功告成，请访问: http://${osip}:6080/#!/settings/rpc/set/ws/${osip}/6800/jsonrpc/${secret_base64url}" >> ~/_ccaa_info_
 	echo -e "File Browser 用户名:${green}${filebrowserUser}${none}"
-	ehco "File Browser 用户名:${filebrowserUser}" >> ~/_ccaa_info_
+	echo "File Browser 用户名:${filebrowserUser}" >> ~/_ccaa_info_
 	echo -e "File Browser 密码:${green}admin${none} (防止别人扫描 建议第一次登录后修改)"
 	echo "File Browser 密码:admin (防止别人扫描 建议第一次登录后修改)" >> ~/_ccaa_info_
 	echo -e "Aria2 RPC 密钥: ${green}${secret}${none}"
